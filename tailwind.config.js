d53d7843a2f947f8375f6a29bcdbe3f73e021b39
Spicer Matthews <github@cloudmanic.com>
2023-03-10 02:38:47 -0800

    Got our promo video in place.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,20 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["content/**/*.md", "layouts/**/*.html", "content/**/*.html", "assets/css/v2.css"],
+
   theme: {
     extend: {
+      colors: {
+        red: {
+          550: "#d64561",
+          850: "#953043",
+        },
+
+        blue: {
+          550: "#2a9eca",
+        },
+      },
+
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
